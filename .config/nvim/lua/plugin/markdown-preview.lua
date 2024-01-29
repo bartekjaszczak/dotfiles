4d41781d3a208ea337bc6d3ae8a7a766f429154d
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2024-01-29 22:51:41 +0100

    nvim: Markdown-preview keybinding

--- .config/nvim/lua/plugin/markdown-preview.lua
+++ .config/nvim/lua/plugin/markdown-preview.lua
@@ -1,8 +1,13 @@
 return {
     "iamcco/markdown-preview.nvim",
+
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    keys = { "<leader>mp" },
     ft = { "markdown" },
     build = function()
         vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+        vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>")
     end,
 }
