ada28a5fc4de772fdddc05f58150b7d4849f644d
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2024-02-25 18:32:50 +0100

    nvim: Replace unsupported formatters

--- .config/nvim/lua/plugin/null-ls.lua
+++ .config/nvim/lua/plugin/null-ls.lua
@@ -10,9 +10,7 @@ return {
                 null_ls.builtins.formatting.clang_format,
                 -- null_ls.builtins.formatting.prettier, -- don't need this if we have prettierd
                 null_ls.builtins.formatting.prettierd,
-                -- null_ls.builtins.code_actions.eslint, -- don't need this if we have eslint_d
-                null_ls.builtins.code_actions.eslint_d,
-                null_ls.builtins.formatting.beautysh,
+                null_ls.builtins.formatting.shfmt,
             },
         })
     end,
