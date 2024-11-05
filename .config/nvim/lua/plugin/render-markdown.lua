f5dae97776b3bc780fb063724b143e5fa4f28abb
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2024-11-05 22:14:45 +0100

    fix(nvim): render-markdown remove headings bg

--- .config/nvim/lua/plugin/render-markdown.lua
+++ .config/nvim/lua/plugin/render-markdown.lua
@@ -5,4 +5,12 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
+
+    config = function()
+        require("render-markdown").setup({
+            heading = {
+                backgrounds = {},
+            },
+        })
+    end,
 }
