82e163d81af035b52a4c711410dd65094f9dce26
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2025-03-03 21:15:21 +0100

    feat(nvim): animated highlights

--- .config/nvim/lua/plugin/local-highlight.lua
+++ .config/nvim/lua/plugin/local-highlight.lua
@@ -1,8 +1,20 @@
 return {
     "tzachar/local-highlight.nvim",
+
+    dependencies = {
+        "folke/snacks.nvim",
+    },
     config = function()
         require("local-highlight").setup({
             min_match_len = 3,
+            animate = {
+                enabled = true,
+                easing = "linear",
+                duration = {
+                    step = 10,
+                    total = 100,
+                },
+            },
         })
     end,
 }
